57780cd30f6053f4a5d6ce485d6246423e83fffe
Cryton Andrew <crytonadrew21@gmail.com>
2021-04-03 10:11:56 +0200

    Pre build

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Row, Col, Container, Image, Button, Carousel} from "react-bootstrap"
+import {Row, Col, Container, Image, Button, Carousel, Form} from "react-bootstrap"
 
 const HomeScreen = () => {
     return (
@@ -117,6 +117,72 @@ const HomeScreen = () => {
                         <Col>
                             <h2>About the Author</h2>
                             <hr style={{backgroundColor: "#ef8172", height: "5px", border: "0px"}}></hr>
+                            <br></br>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur
+                             adipiscing elit. Praesent sollicitudin magna vitae ullamcorper venenatis. 
+                             Duis hendrerit, risus vitae tempor volutpat, 
+                            leo justo vulputate ex, vel semper diam nulla ut massa. 
+                            Integer fringilla ipsum sit amet gravida euismod
+                            </p>
+                            <br></br>
+                            <br></br>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur
+                             adipiscing elit. Praesent sollicitudin magna vitae ullamcorper venenatis. 
+                             Duis hendrerit, risus vitae tempor volutpat, 
+                            leo justo vulputate ex, vel semper diam nulla ut massa. 
+                            Integer fringilla ipsum sit amet gravida euismod
+                            </p>
+                            <br></br>
+                            <br></br>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur
+                             adipiscing elit. Praesent sollicitudin magna vitae ullamcorper venenatis. 
+                             Duis hendrerit, risus vitae tempor volutpat, 
+                            leo justo vulputate ex, vel semper diam nulla ut massa. 
+                            Integer fringilla ipsum sit amet gravida euismod
+                            </p>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            {/* Contact Section  */}
+
+
+            <section className="contact-section">
+                <Container>
+                    <h1>Contact Me</h1>
+
+                    <Row>
+                        <Col md={8}>
+                        <hr></hr>
+                        <Form>
+                        <Form.Group controlId="formGroupEmail">
+                            <Form.Label>Full Name:</Form.Label>
+                            <Form.Control type="email" placeholder="Enter full name" />
+                        </Form.Group>
+                        <br></br>
+                        <Form.Group controlId="formGroupPassword">
+                            <Form.Label>Email Address:</Form.Label>
+                            <Form.Control type="password" placeholder="Enter Email" />
+                        </Form.Group>
+                        <br></br>
+                        <Form.Group controlId="formGroupPassword">
+                            <Form.Label>Message</Form.Label>
+                            <Form.Control as="textArea" type="text" placeholder="Message" rows={4}/>
+                        </Form.Group>
+                        </Form>
+                        </Col>
+                        <Col className="info-col" md={4}>
+                            <h2>Info</h2>
+                            <br></br>
+                            <h6><i className="fas fa-envelope"></i>  Email: </h6>
+                            <br></br>
+                            <h6><i className="fab fa-instagram"></i>  Instagram: </h6>
+                            <br></br>
+                            <h6><i className="fas fa-clock"></i>  09:00 - 18:00</h6>
                         </Col>
                     </Row>
                 </Container>
